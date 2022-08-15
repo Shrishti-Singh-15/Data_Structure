b5b031e92a980c61e1447751036c8119641d0a1e
Shrishti_Singh_15 <shrishti.singh@watermelon.us>
2022-08-15 23:53:10 +0530

    commit

---
+++ Arrays/Maximum_Subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxsum = 0;
+        int currsum = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            currsum = currsum + nums[i];
+            if(currsum > maxsum)
+            {
+                maxsum = currsum;
+            }
+            if(currsum < 0)
+            {
+                currsum = 0;
+            }
+        }
+        return maxsum;
+    }
+}
